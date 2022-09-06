e0e960a60809123f7710bbf1dc845b0592efa5f3
OliveiraRafaella <rafaella.vitorino2001@gmail.com>
2022-09-06 13:28:15 -0300

    Servicos Empresa

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
         <div class= "container" id="nav-container">
             <nav class="navbar navbar-expand-lg fixed-top">
                 <a href="#" class="navbar-brand">
-                    <img id="logo" src="img/hdcagency_logo.svg" alt="hDC Agency"> hDC Agency
+                    <img id="logo" src="img/hdcagency_logo.svg" alt="Rafa Agency"> Rafa's Agency
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" 
                     arial-controls= "navbar-links" arial-expanded="false" arial-label="Toggle navigation">
@@ -47,8 +47,8 @@
         </div>
     </header>
     <main> 
-        <div class="container-fluid"> <!--Caroussel Imagens-->
-            <div id="mainSlider" class="carousel slide" data-ride="carousel">
+        <div class="container-fluid"> 
+            <div id="mainSlider" class="carousel slide" data-ride="carousel"> <!--Caroussel Imagens-->
                 <ol class="carousel-indicators">
                     <li data-target="#mainSlider" data-slide-to="0" class="active"></li>
                     <li data-target="#mainSlider" data-slide-to="1">
@@ -75,7 +75,7 @@
                         <img src="img/banner3.png" class="d-block w-100" alt="Manutenção em Software">
                         <div class="carousel-caption d-nome d-md-block">
                             <h2>Está buscando manutenção no seu software?</h2>
-                            <p>A hDC conta com engenheiros altamentes qualificados para rexolver seu problema.</p>
+                            <p>A Rafa's conta com engenheiros altamentes qualificados para rexolver seu problema.</p>
                             <a href="#" class="main-btn">Entre em contato</a>
                         </div>
                     </div>
@@ -89,14 +89,14 @@
                     <span class="sr-only">Next</span>
                 </a>
             </div>
-            <div id="about-area"> <!-- -->
+            <div id="about-area"> <!--Sobre a empresa-->
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
-                            <h3 class="main-title">Sobre a hDC Agency</h3>
+                            <h3 class="main-title">Sobre a Rafa's Agency</h3>
                         </div>
                         <div class="col-md-6">
-                            <img class="img-fluid" src="img/agencia.jpg" alt="Agência hDC">
+                            <img class="img-fluid" src="img/agencia.jpg" alt="Agência Rafa">
                         </div>
                         <div class="col-md-6">
                             <h3 class="about-title">Uma agência que pensa no futuro</h3>
@@ -115,7 +115,45 @@
                     </div>
                 </div>
             </div>
-                    
+            <div id="service-area"><!--Serviços da empresa-->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <h3 class="main-title">Nossas especialidades</h3>
+                        </div>
+                        <div class="col-md-4 service-box">
+                            <i class="fas fa-mobile-alt"></i>
+                            <h4>Aplicativos Nativos</h4>
+                            <p>Utilizamos o real poder dos dispositivos móveis construindo aplicativos nativos.</p>
+                        </div>
+                        <div class="col-md-4 service-box">
+                            <i class="fas fa-shopping-cart"></i>
+                            <h4>E-commerces</h4>
+                            <p>Temos uma plataforma própria de e-commerce que se adapta a qualquer regra de negócio (B2C e B2B).</p>
+                        </div>
+                        <div class="col-md-4 service-box">
+                            <i class="fas fa-paint-brush"></i>
+                            <h4>Design Criativo</h4>
+                            <p>Nossos designers são especializados em UI/UX e também possuem vasta experiência no mercado de software.</p>
+                        </div>
+                        <div class="col-md-4 service-box">
+                            <i class="fab fa-google"></i>
+                            <h4>Experts em SEO</h4>
+                            <p>Com o nosso serviço de marketing seu site será melhor posicionado no Google e venderá mais.</p>
+                        </div>
+                        <div class="col-md-4 service-box">
+                            <i class="fas fa-hands-helping"></i>
+                            <h4>Suporte 24/7</h4>
+                            <p>Você sempre terá alguem disponível para suporte, em diferentes plataformas. (chat, email, skype).</p>
+                        </div>
+                        <div class="col-md-4 service-box">
+                            <i class="fas fa-server"></i>
+                            <h4>Experiência em servidores</h4>
+                            <p>Garantimos uptime e também rápido tempo de resposta do seu site para o seu cliente.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </main> 
     <script src="js/scripts.js"></script>
