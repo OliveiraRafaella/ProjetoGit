428e18df8e95dfed2d63d8f63bf245fd99edadae
OliveiraRafaella <rafaella.vitorino2001@gmail.com>
2022-09-08 14:56:43 -0300

    News letter e call action

--- index.php
+++ index.php
@@ -235,7 +235,7 @@
                     </div>
                 </div>
             </div>
-            <div class="portifolio-area"> <!--Portifolio-->
+            <div id="portifolio-area"> <!--Portifolio-->
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
@@ -265,6 +265,26 @@
                         <div class="col-md-4 project-box seo">
                             <img src="img/proj6.jpg" class="img-fluid" alt="Projeto 6">
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div id="news-area"> <!--Newsletter-->
+                <div class="container">
+                    <div class="col-md-12">
+                        <h3 class="main-title">Fique por dentro das novidades</h3>
+                    </div>
+                    <p>Assine nossa lista de e-mails, e receba nossos conteúdos sobre desenvolvimento de software</p>
+                    <form action="">
+                        <input type="email" class="form-control" id="email-input" name="email" placeholder="Seu melhor e-mail">
+                        <input type="submit" id="news-btn" value="Inscrever">
+                    </form>
+                </div>
+            </div>
+            <div id="call-area"> <!--Call to action-->
+                <div class="container">
+                    <div class="row">
+                        <p>Deseja fazer um orçamento sem compromisso</p>
+                        <button class="main-btn" id="call-btn">Orçar</button>
                     </div>
                 </div>
             </div>
